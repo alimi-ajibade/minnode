b296e8b6313b94ffaabb6755e0d355071dba5a49
HelloSolo <davidalimi148@gmail.com>
2024-04-18 10:15:03 +0100

    remove login and sign button from login and signup page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { SectionWrapper } from "./hoc";
 import { IoMdMenu } from "react-icons/io";
 import { MdClose } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const NavBar = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const { pathname } = useLocation();
     const navigate = useNavigate();
+    console.log(pathname);
 
     return (
         <nav className={`flex flex-row items-center justify-between p-2`}>
@@ -25,19 +27,25 @@ const NavBar = () => {
                 <span className="font-bold">SimpleMind</span>
             </div>
 
-            <div className="hidden lg:flex flex-row justify-between gap-2">
-                <button
-                    className="p-2 w-28 font-bold border-1 rounded-md outline outline-1 outline-neutral-700 hover:bg-purple-400 active:bg-purple-600 transition duration-500 ease-in-out"
-                    onClick={() => navigate("/login")}>
-                    Login
-                </button>
-                <button
-                    className="p-2 w-28 font-bold border-1 rounded-md outline outline-1 outline-neutral-700 bg-purple-200 hover:bg-purple-400 active:bg-purple-600 transition duration-500 ease-in-out"
-                    onClick={() => navigate("/sign-up")}>
-                    Sign up
-                </button>
-            </div>
+            {pathname.includes("login") === false &&
+            pathname.includes("sign-up") === false ? (
+                <div className="hidden lg:flex flex-row justify-between gap-2">
+                    <button
+                        className="p-2 w-28 font-bold border-1 rounded-md outline outline-1 outline-neutral-700 hover:bg-purple-400 active:bg-purple-600 transition duration-500 ease-in-out"
+                        onClick={() => navigate("/login")}>
+                        Login
+                    </button>
+                    <button
+                        className="p-2 w-28 font-bold border-1 rounded-md outline outline-1 outline-neutral-700 bg-purple-200 hover:bg-purple-400 active:bg-purple-600 transition duration-500 ease-in-out"
+                        onClick={() => navigate("/sign-up")}>
+                        Sign up
+                    </button>
+                </div>
+            ) : (
+                <></>
+            )}
 
+            {/* Mobile */}
             <div
                 className={`lg:hidden absolute inset-0 h-screen w-2/3 bg-purple-50 text-xl duration-300 ease-out transition-all ${
                     showMenu ? "translate-x-0" : "-translate-x-full"
