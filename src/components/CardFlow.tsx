a41e5929a7f175536d94a291e0fd43971aeb2583
HelloSolo <davidalimi148@gmail.com>
2024-04-28 18:47:46 +0100

    fix card flow

--- src/components/CardFlow.tsx
+++ src/components/CardFlow.tsx
@@ -1,11 +1,58 @@
-import { Background, ReactFlow, ReactFlowProvider } from "reactflow";
+import {
+    Background,
+    Handle,
+    NodeProps,
+    Position,
+    ReactFlow,
+    ReactFlowProvider,
+} from "reactflow";
 import MindMap from "../entities/Mindmap";
-import CustomNode from "./MindMap/CustomNode";
+import { NodeData } from "./MindMap/store";
 import CustomEdge from "./MindMap/CustomEdge";
 
 interface Props {
     mindmap: MindMap;
 }
+
+const CustomNode = ({ data }: NodeProps<NodeData>) => {
+    return (
+        <>
+            <div>
+                <input
+                    defaultValue={data.label}
+                    style={{
+                        backgroundColor: data.backgroundColor,
+                    }}
+                    className="p-2 text-center border focus:outline-none"
+                />
+                <Handle
+                    type="target"
+                    position={Position.Top}
+                    id="targetT"
+                    className="opacity-0"
+                />
+                <Handle
+                    type="source"
+                    position={Position.Bottom}
+                    id="sourceB"
+                    className="opacity-0"
+                />
+                <Handle
+                    type="source"
+                    position={Position.Right}
+                    id="sourceR"
+                    className="opacity-0"
+                />
+                <Handle
+                    type="target"
+                    position={Position.Left}
+                    id="targetL"
+                    className="opacity-0"
+                />
+            </div>
+        </>
+    );
+};
 
 const nodeTypes = {
     mindmap: CustomNode,
@@ -16,14 +63,17 @@ const edgeTypes = {
 };
 
 const CardFlow = ({ mindmap }: Props) => {
+    const { nodes, edges } = mindmap;
+
     return (
         <ReactFlowProvider>
             <ReactFlow
-                nodes={mindmap.nodes}
-                edges={mindmap.edges}
+                id={"name"}
+                nodes={nodes}
+                edges={edges}
                 nodeTypes={nodeTypes}
                 edgeTypes={edgeTypes}
-                fitView>
+                fitView={true}>
                 <Background />
             </ReactFlow>
         </ReactFlowProvider>
