69fc01dd4655eb4cbfed6129e74b885c62c8aa83
HelloSolo <davidalimi148@gmail.com>
2024-04-29 16:38:00 +0100

    adjust dropdown placement

--- src/components/Dropdowns.tsx
+++ src/components/Dropdowns.tsx
@@ -15,7 +15,7 @@ const Dropdowns = ({ mindmap }: Props) => {
     return (
         <div
             id={mindmap._id}
-            className={`dropdowns hidden z-20 absolute bg-white shadow-md rounded-md top-56 left-40`}>
+            className={`dropdowns hidden z-30 absolute bg-white shadow-md rounded-md top-56 -right-5`}>
             <ul className="w-20">
                 <li className="cursor-pointer hover:bg-gray-200">
                     <button
