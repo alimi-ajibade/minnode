b1b6b732db07dfa972a228a45143721150e9d7fb
HelloSolo <davidalimi148@gmail.com>
2024-04-29 15:37:13 +0100

    Fix: undefined in last modified

--- src/utils/getTimeDifference.ts
+++ src/utils/getTimeDifference.ts
@@ -18,7 +18,9 @@ const getTimeDiff = (last_modified: string) => {
     if (diffDays > 0) return format(new Date(last_modified), "MMM dd");
     if (diffHours > 0) return `${diffHours} hour(s) ago`;
     if (diffMinutes > 0) return `${diffMinutes} minute(s) ago`;
-    if (diffSeconds > 0) return `${diffSeconds} second(s) ago`;
+    if (diffSeconds === 0) return `now`;
+
+    return `${diffSeconds} second(s) ago`;
 };
 
 export default getTimeDiff;
