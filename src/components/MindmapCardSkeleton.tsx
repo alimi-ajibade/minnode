08a07a4175ab993afe1966fab318f966c9cd746f
HelloSolo <davidalimi148@gmail.com>
2024-05-03 18:11:27 +0100

    build loading skeleton

---
+++ src/components/MindmapCardSkeleton.tsx
@@ -0,0 +1,13 @@
+const MindmapCardSkeletont = () => {
+    return (
+        <div className="relative flex flex-col columns-1 rows-2 h-64 rounded-md bg-gray-300 animate-pulse">
+            <div className="min-h-48 min-w-full rounded-md bg-gray-200"></div>
+            <div className="p-2">
+                <div className="w-1/2 h-3 rounded bg-gray-200"></div>
+                <div className="w-[80%] h-3 mt-2 rounded bg-gray-200"></div>
+            </div>
+        </div>
+    );
+};
+
+export default MindmapCardSkeletont;
--- src/components/MindmapGrid.tsx
+++ src/components/MindmapGrid.tsx
@@ -1,10 +1,21 @@
 import useMindmaps from "../hooks/useMindmaps";
 import MindmapCard from "./MindmapCard";
+import MindmapCardSkeletont from "./MindmapCardSkeleton";
 
 const MindmapGrid = () => {
-    const { data: mindmaps } = useMindmaps();
+    const { data: mindmaps, isLoading } = useMindmaps();
+    const skeletons = [...Array(2).keys()];
 
     if (mindmaps?.length === 0) return <></>;
+
+    if (isLoading)
+        return (
+            <div className="grid grid-cols-1 gap-5 lg:grid-cols-4 xl:grid-cols-6 mt-5 py-5 px-5 bg-white rounded-md min-w-full">
+                {skeletons.map((index) => (
+                    <MindmapCardSkeletont key={index} />
+                ))}
+            </div>
+        );
 
     return (
         <div
