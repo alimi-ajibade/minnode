f38a0e1ee80f76f0eb1c621be4e529a8229b8757
HelloSolo <davidalimi148@gmail.com>
2024-05-07 07:52:23 +0100

    debuging: api url

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios, { CanceledError } from "axios";
 const APIURL = import.meta.env.VITE_MINDMAP_API_URL;
 
 export default axios.create({
-    baseURL: APIURL,
+    baseURL: "https://mindmap-api.onrender.com/api",
 });
 
 export { CanceledError };
--- src/services/socket-client.ts
+++ src/services/socket-client.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 const APIURL = import.meta.env.VITE_MINDMAP_API_URL;
 
-const socket = io(APIURL, {
+const socket = io("https://mindmap-api.onrender.com/api", {
     autoConnect: false,
     reconnectionDelay: 10000,
 });
